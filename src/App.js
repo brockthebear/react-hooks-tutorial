be50f8854f736acbc86426acf0a6a02ae833d43f
Brock Boren <brock.boren@gmail.com>
2020-02-22 18:54:44 -0600

    Context with Hooks

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React, { useRef } from 'react';
+import React, { useRef, createContext } from 'react';
 import Toggle from './Toggle';
 import { useTitleInput } from './hooks/useTitleInput';
+
+export const UserContext = createContext();
 
 const App = () => {
 	const [name, setName] = useTitleInput('');
 	const appRef = useRef();
 
 	return (
-		<div className='main-wrapper' ref={appRef}>
-			<h1
-				onClick={() => {
-					appRef.current.classList.add('new-fake-class');
-				}}
-			>
-				Level Up Dishes
-			</h1>
-			<Toggle />
-			<form
-				onSubmit={e => {
-					e.preventDefault();
-				}}
-			>
-				<input type='text' onChange={e => setName(e.target.value)} value={name} />
-				<button>Submit</button>
-			</form>
-		</div>
+		<UserContext.Provider
+			value={{
+				user: false,
+			}}
+		>
+			<div className='main-wrapper' ref={appRef}>
+				<h1
+					onClick={() => {
+						appRef.current.classList.add('new-fake-class');
+					}}
+				>
+					Level Up Dishes
+				</h1>
+				<Toggle />
+				<form
+					onSubmit={e => {
+						e.preventDefault();
+					}}
+				>
+					<input type='text' onChange={e => setName(e.target.value)} value={name} />
+					<button>Submit</button>
+				</form>
+			</div>
+		</UserContext.Provider>
 	);
 };
 
