5241de1d22541683ddcedcdc4d578a90a16c215e
Brock Boren <brock.boren@gmail.com>
2020-02-22 19:21:13 -0600

    useMemo for expensive functions.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React, { useRef } from 'react';
-import Counter from './Counter';
+import React, { useRef, useMemo } from 'react';
 import Toggle from './Toggle';
 import { useTitleInput } from './hooks/useTitleInput';
 
 const App = () => {
 	const [name, setName] = useTitleInput('');
 	const appRef = useRef();
+	const title = 'Level Up Dishes';
+
+	const reverseWord = word => {
+		console.log(`reverse ${word === title ? 'title' : 'word'}`);
+		return word
+			.split('')
+			.reverse()
+			.join('');
+	};
+
+	// useMemo caches the `title` value and checks its value.
+	// if it has not changed, the `reverseWord()` function is not run.
+	//
+	// useMemo is best used when you have expensive functions that you only want to run under specific circumstances.
+	// very similar to shouldComponentUpdate().
+	useMemo(() => reverseWord(name), [name]);
+	const TitleReversed = useMemo(() => reverseWord(title), [title]);
 
 	return (
 		<div className='main-wrapper' ref={appRef}>
@@ -14,10 +30,9 @@ const App = () => {
 					appRef.current.classList.add('new-fake-class');
 				}}
 			>
-				Level Up Dishes
+				{TitleReversed}
 			</h1>
 			<Toggle />
-			<Counter />
 			<form
 				onSubmit={e => {
 					e.preventDefault();
