50f3110b11752402415dff13e2d2dd36dc3c1ec1
Brock Boren <brock.boren@gmail.com>
2020-02-22 18:21:32 -0600

    useState part 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Toggle from './Toggle';
 
 const App = () => {
-  return (
-    <div className="main-wrapper">
-      <h1>Level Up Dishes</h1>
-    </div>
-  );
+	const [name, setName] = useState('');
+
+	return (
+		<div className='main-wrapper'>
+			<h1>Level Up Dishes</h1>
+			<Toggle />
+			<input type='text' onChange={e => setName(e.target.value)} value={name} />
+		</div>
+	);
 };
 
 export default App;
