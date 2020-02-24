6a76c900c1fda6a6025d224b19ed2f5a6bbb5041
Brock Boren <brock.boren@gmail.com>
2020-02-23 19:42:00 -0600

    Basic animation with react spring using the useSpring hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import useAbortableFetch from 'use-abortable-fetch';
+import { useSpring, animated } from 'react-spring';
 import Toggle from './Toggle';
 import { useTitleInput } from './hooks/useTitleInput';
 
@@ -10,21 +11,25 @@ const App = () => {
 	const appRef = useRef();
 	const [name, setName] = useTitleInput('');
 
+	const styleProps = useSpring({
+		opacity: 1,
+		from: { opacity: 0 },
+	});
+
 	const { data, loading, error, abort } = useAbortableFetch(api);
 
-	if (loading) return <div>Loading...</div>;
 	if (error) return <div>Error: {error.message}</div>;
-	if (!data) return null;
 
 	return (
 		<div className='main-wrapper' ref={appRef}>
-			<h1
+			<animated.h1
+				style={styleProps}
 				onClick={() => {
 					appRef.current.classList.add('new-fake-class');
 				}}
 			>
 				{title}
-			</h1>
+			</animated.h1>
 			<Toggle />
 			<form
 				onSubmit={e => {
@@ -35,17 +40,18 @@ const App = () => {
 				<button>Submit</button>
 			</form>
 
-			{data.map(dish => (
-				<article className='dish-card dish-card--withImage'>
-					<h3>{dish.name}</h3>
-					<p>{dish.desc}</p>
-					<div className='ingredients'>
-						{dish.ingredients.map(ingredient => (
-							<span>{ingredient}</span>
-						))}
-					</div>
-				</article>
-			))}
+			{data &&
+				data.map(dish => (
+					<article className='dish-card dish-card--withImage'>
+						<h3>{dish.name}</h3>
+						<p>{dish.desc}</p>
+						<div className='ingredients'>
+							{dish.ingredients.map(ingredient => (
+								<span>{ingredient}</span>
+							))}
+						</div>
+					</article>
+				))}
 		</div>
 	);
 };
